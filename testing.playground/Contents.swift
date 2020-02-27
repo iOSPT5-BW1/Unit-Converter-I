de9ce72f851c0cdec0e1da6b664e1cae410f7879
Chad Parker <chad@chadparker.com>
2020-02-26 19:11:35 -0800

    Add playground

---
+++ testing.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+struct Unit {
+
+    let name: String
+    let isHowManyInches: Double
+}
+
+let units: [Unit] = [
+    Unit(name: "Inches", isHowManyInches: 1),
+    Unit(name: "Feet", isHowManyInches: 12),
+    Unit(name: "Centimeters", isHowManyInches: 0.393701)
+]
