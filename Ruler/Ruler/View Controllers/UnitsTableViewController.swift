6226a87ab54c30d90e3c022cc89b0594bd7c5ec3
Chad Parker <chad@chadparker.com>
2020-02-27 19:42:07 -0800

    Clean up

--- Ruler/Ruler/View Controllers/UnitsTableViewController.swift
+++ Ruler/Ruler/View Controllers/UnitsTableViewController.swift
@@ -16,13 +16,10 @@ class UnitsTableViewController: UITableViewController {
         super.viewDidLoad()
     }
 
-    // MARK: - Table view data source
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return unitController.units.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "UnitCell", for: indexPath) as! UnitTableViewCell
 
@@ -30,16 +27,6 @@ class UnitsTableViewController: UITableViewController {
         cell.delegate = self
         return cell
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
 extension UnitsTableViewController: CellDelegate {
@@ -47,7 +34,6 @@ extension UnitsTableViewController: CellDelegate {
     func valueDidChange(unit: Unit) {
         print("value changed to: \(unit.currentValue)")
         print("value in inches:  \(unit.currentValue * unit.isHowManyInches)")
-
         NotificationCenter.default.post(name: .valueHasChanged, object: nil)
     }
 }
