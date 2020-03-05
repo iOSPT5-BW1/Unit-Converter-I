519561315b41fb968659d695960c7fa2e4a83ba1
Chad Parker <chad@chadparker.com>
2020-03-04 20:37:34 -0800

    Store currentValueInMeters too, rewrite set/getValue for better accuracy

--- Ruler/Ruler/Model/Unit.swift
+++ Ruler/Ruler/Model/Unit.swift
@@ -17,5 +17,13 @@ struct Unit {
 
     let name: String
     let isHowManyInches: Double
+    let isHowManyMeters: Double
     let type: UnitType
+
+    init(name: String, isHowManyInches: Double, isHowManyMeters: Double = 0, type: UnitType) {
+        self.name = name
+        self.isHowManyInches = isHowManyInches
+        self.isHowManyMeters = isHowManyMeters
+        self.type = type
+    }
 }
--- Ruler/Ruler/Model/UnitController.swift
+++ Ruler/Ruler/Model/UnitController.swift
@@ -11,27 +11,52 @@ import Foundation
 class UnitController {
 
     let units: [Unit] = [
-        Unit(name: "mm", isHowManyInches: 0.0393700787, type: .metric),
-        Unit(name: "cm", isHowManyInches: 0.39370787, type: .metric),
+        Unit(name: "mm", isHowManyInches: 0.0393700787, isHowManyMeters: 0.001, type: .metric),
+        Unit(name: "cm", isHowManyInches: 0.39370787, isHowManyMeters: 0.01, type: .metric),
         Unit(name: "inches", isHowManyInches: 1, type: .imperial),
         Unit(name: "feet", isHowManyInches: 12, type: .imperial),
-        Unit(name: "km", isHowManyInches: 39370.0787, type: .metric),
+        Unit(name: "m", isHowManyInches: 39.3701, isHowManyMeters: 1, type: .metric),
+        Unit(name: "km", isHowManyInches: 39370.0787, isHowManyMeters: 1000, type: .metric),
         Unit(name: "miles", isHowManyInches: 63360, type: .imperial),
     ]
 
     // MARK: - State
 
-    private var currentValueInInches: Double = 0
+    private var currentValueInInches: Double = 0 // this is non-zero if input is an imperial unit
+    private var currentValueInMeters: Double = 0 // this is non-zero if input is a metric unit
 
     // MARK: - Core calculations
 
     private func setValue(_ newValue: Double, for unit: Unit) {
-        currentValueInInches = newValue * unit.isHowManyInches
+        switch unit.type {
+        case .imperial:
+            currentValueInInches = newValue * unit.isHowManyInches
+            currentValueInMeters = 0
+        case .metric:
+            currentValueInMeters = newValue * unit.isHowManyMeters
+            currentValueInInches = 0
+        }
         NotificationCenter.default.post(name: .valueHasChanged, object: nil)
     }
 
     private func value(for unit: Unit) -> Double {
-        return currentValueInInches / unit.isHowManyInches
+        if currentValueInInches != 0 { // imperial
+            switch unit.type {
+            case .imperial:
+                return currentValueInInches / unit.isHowManyInches
+            case .metric:
+                let valueInMeters = currentValueInInches * 0.0254
+                return valueInMeters / unit.isHowManyMeters
+            }
+        } else { // metric
+            switch unit.type {
+            case .imperial:
+                let valueInInches = currentValueInMeters / 0.0254
+                return valueInInches / unit.isHowManyInches
+            case .metric:
+                return currentValueInMeters / unit.isHowManyMeters
+            }
+        }
     }
 
     // MARK: - Conversions to/from string
@@ -49,7 +74,7 @@ class UnitController {
         formatter.maximumFractionDigits = SettingsController.decimalPlaces
         formatter.numberStyle = .decimal
         let string = formatter.string(from: NSNumber(value: value(for: unit))) ?? ""
-        if currentValueInInches != 0 {
+        if currentValueInInches != 0 || currentValueInMeters != 0 {
             if string == "0" { // too small to display
                 return "-"
             } else {
